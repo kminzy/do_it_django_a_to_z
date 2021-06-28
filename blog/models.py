43d651c0bbc849694d02697f1f885994b2861e03
kminzy <kminzy0704@gmail.com>
2021-06-28 14:25:41 +0900

    포스트 목록, 상세 페이지에서 대표 이미지 없을 때 문제 해결하기

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=30)
@@ -10,7 +11,8 @@ class Post(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    #author: 추후 작성 예정
+
+    # author: 추후 작성 예정
 
     def __str__(self):
         return f'[{self.pk}] {self.title}'
