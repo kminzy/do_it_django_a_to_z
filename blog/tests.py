9dccb7c9345b4de0153e6e894bbbdba72e9af62b
kminzy <kminzy0704@gmail.com>
2021-07-02 11:37:32 +0900

    포스트 상세 페이지에 카테고리 기능 추가

--- blog/tests.py
+++ blog/tests.py
@@ -17,13 +17,21 @@ class TestView(TestCase):
         self.category_music = Category.objects.create(name='music', slug='music')
 
         self.post_001 = Post.objects.create(
-            title='첫 번째 포스트입니다.', content='Hello World. We are the world.', author=self.user_trump
+            title='첫 번째 포스트입니다.',
+            content='Hello World. We are the world.',
+            category=self.category_programming,
+            author=self.user_trump
         )
         self.post_002 = Post.objects.create(
-            title='두 번째 포스트입니다.', content='1등이 전부는 아니잖아요?', author=self.user_obama
+            title='두 번째 포스트입니다.',
+            content='1등이 전부는 아니잖아요?',
+            category=self.category_music,
+            author=self.user_obama
         )
         self.post_003 = Post.objects.create(
-            title='세 번째 포스트입니다.', content='category가 없을 수도 있죠', author=self.user_obama
+            title='세 번째 포스트입니다.',
+            content='category가 없을 수도 있죠',
+            author=self.user_obama
         )
 
     def category_card_test(self, soup):
@@ -33,7 +41,7 @@ class TestView(TestCase):
                       categories_card.text)
         self.assertIn(f'{self.category_music.name} ({self.category_music.post_set.count()})',
                       categories_card.text)
-        self.assertIn(f'미분류 (3)', categories_card.text)
+        self.assertIn(f'미분류 (1)', categories_card.text)
 
     def navbar_test(self, soup):
         navbar = soup.nav
@@ -82,7 +90,7 @@ class TestView(TestCase):
 
         post_003_card = main_area.find('div', id='post-3')
         self.assertIn('미분류', post_003_card.text)
-        self.assertIn(self.post_003.category.name, post_003_card.text)
+        self.assertIn(self.post_003.title, post_003_card.text)
 
         self.assertIn(self.user_trump.username.upper(), main_area.text)
         self.assertIn(self.user_obama.username.upper(), main_area.text)
@@ -122,28 +130,26 @@ class TestView(TestCase):
 
     def test_post_detail(self):
         # 1.1 포스트가 하나 있다
-        post_001 = Post.objects.create(
-            title='첫 번째 포스트입니다.', content='Hello World. We are the world.', author=self.user_trump,
-        )
         # 1.2 그 포스트의 url은 'blog/1/' 이다.
-        self.assertEqual(post_001.get_absolute_url(), '/blog/1/')
+        self.assertEqual(self.post_001.get_absolute_url(), '/blog/1/')
 
         # 2 첫 번째 포스트의 상세 페이지 테스트
         # 2.1 첫 번째 포스트의 url로 접근하면 정상적으로 작동한다(status code: 200).
-        response = self.client.get(post_001.get_absolute_url())
+        response = self.client.get(self.post_001.get_absolute_url())
         self.assertEqual(response.status_code, 200)
         soup = BeautifulSoup(response.content, 'html.parser')
         # 2.2 포스트 목록 페이지와 똑같은 navbar가 있다
         self.navbar_test(soup)
+        self.category_card_test(soup)
         # 2.3 첫 번째 포스트의 제목이 웹 브라우저 탭 타이틀에 들어 있다
-        self.assertIn(post_001.title, soup.title.text)
+        self.assertIn(self.post_001.title, soup.title.text)
         # 2.4 첫 번째 포스트의 제목이 포스트 영역에 있다
         main_area = soup.find('div', id='main-area')
         post_area = main_area.find('div', id='post-area')
-        self.assertIn(post_001.title, post_area.text)
+        self.assertIn(self.post_001.title, post_area.text)
+        self.assertIn(self.category_programming.name, post_area.text)
         # 2.5 첫 번째 포스트의 작성자(author)가 포스트 영역에 있다(아직 구현할 수 없다)
         # 아직 작성 불가
         # 2.6 첫 번째 포스트의 내용(content)이 포스트 영역에 있다
-        self.assertIn(post_001.content, post_area.text)
-
-        self.assertIn(post_001.content, post_area.text)
+        self.assertIn(self.user_trump.username.upper(), post_area.text)
+        self.assertIn(self.post_001.content, post_area.text)
